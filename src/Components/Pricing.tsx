1234e2871e061b7396903264071098679e824857
Gaurav Sharma <gauravsharma15112@gmail.com>
2024-05-01 13:24:12 +0530

    first commit

--- src/Components/Pricing.tsx
+++ src/Components/Pricing.tsx
@@ -20,51 +20,48 @@ const Pricing = () => {
         <div className="price_content">
           {/* internal wrapper cover whole card  */}
           <div className="price_content__wrapper">
-            <div>
-              <div className="price_content__wrapper__price">
-                <div className="price_content__wrapper__price__page">
-                  <div className="pagview">100k PAGEVIEWS</div>
-                </div>
-                <div className="price_content__wrapper__price__div">
-                  <div className="dollar">
-                    $16.00 <span>/month</span>
-                  </div>
-                </div>
-              </div>
-
-              {/* progress bar section */}
-              <div className="price_content__wrapper__price_progressbar">
-                <div className="progres">
-                  <input
-                    type="range"
-                    name="slider"
-                    id="slider"
-                    min="0"
-                    max="100"
-                  />
-                </div>
-              </div>
-
-              {/* end of progress bar */}
-
-              <div className="price_content__wrapper_monthly-billing">
-                <div className="Bill">Monthly Billing</div>
-                <div className="off-btn">
-                  <label className="switch">
-                    <input type="checkbox" />
-                    <span className="slider round"></span>
-                  </label>
-                </div>
-                <div className="yearlbillin">Yearly Billing</div>
-                <div className="price_content__wrapper_monthly-billing__Discount-btn">
-                  <div className="dsbtn">25% discount</div>
-                </div>
+            {/* <div className="price_content__wrapper__hraboveCont"> */}
+            <div className="price_content__wrapper__price">
+              <div className="price_content__wrapper__price__page">
+                <div className="pagview"> 100k PAGEVIEWS</div>
               </div>
             </div>
+            <div className="price_content__wrapper__price__div">
+              <div className="dollar">
+                $16.00 <span>/month</span>
+              </div>
+            </div>
+            {/* progress bar section */}
+            <div className="price_content__wrapper__price_progressbar">
+              <div className="progres">
+                <input
+                  type="range"
+                  name="slider"
+                  id="slider"
+                  min="0"
+                  max="100"
+                />
+              </div>
+            </div>
+            {/* end of progress bar */}
+            <div className="price_content__wrapper_monthly-billing">
+              <div className="Bill">Monthly Billing</div>
+              <div className="off-btn">
+                <label className="switch">
+                  <input type="checkbox" />
+                  <span className="slider round"></span>
+                </label>
+              </div>
+              <div className="yearlbillin">Yearly Billing</div>
+              <div className="price_content__wrapper_monthly-billing__Discount-btn">
+                <div className="dsbtn">25% discount</div>
+              </div>
+            </div>
+            {/* </div> */}
+            {/* ....... */}
             <div className="price_content__wrapper_line">
               <hr />
             </div>
-
             <div className="price_content__wrapper_iconbtncont">
               <div className="price_content__wrapper_iconbtncont__benef">
                 <div className="tickandt">
@@ -87,7 +84,6 @@ const Pricing = () => {
                   <div className="text">Email reports </div>
                 </div>
               </div>
-
               <div className="price_content__wrapper_iconbtncont_iconbtn">
                 <div className="trialbtn">Start my trial</div>
               </div>
